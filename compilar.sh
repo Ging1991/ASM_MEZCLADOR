c03bfb3999328776281cdf99d8281b525818c8c6
Ging1991 <carlos_velez10@hotmail.com>
2021-10-31 23:09:54 -0300

    Version final

--- compilar.sh
+++ compilar.sh
@@ -1,3 +1,3 @@
 nasm -f elf32 enmascarar_asm.asm -o enmascarar_asm.o;
 gcc -m32 -o main enmascarar_asm.o main.c;
-./main "imagen1.rgb" "imagen2.rgb" "mascara.rgb";
+./main "imagenes/RGB/imagen1.rgb" "imagenes/RGB/imagen2.rgb" "imagenes/RGB/mascara.rgb";
